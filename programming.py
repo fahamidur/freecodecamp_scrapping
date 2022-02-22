9fe64fd3b9d8757fd84cbad70545d088590da9c1
Md. Fahamidur Rahaman Rafi <44564445+fahamidur@users.noreply.github.com>
2022-02-22 15:40:58 +0600

    Add files via upload

---
+++ programming.py
@@ -0,0 +1,33 @@
+import requests
+from bs4 import BeautifulSoup
+import time
+
+h_url = 'https://www.freecodecamp.org/news/tag/programming/'
+html_text = requests.get(h_url).text
+soup = BeautifulSoup(html_text, 'lxml')
+
+a = soup.find_all('h2',class_="post-card-title")
+f = open("Extracted Files/programming.txt", "a")
+print("Extracting resources.Please Wait.....")
+time.sleep(3)
+item_count = 0
+for a in a:
+    x = a.find('a')
+    title = x.text.strip()
+    y = str(x)
+    url = "freecodecamp.com"
+    for y in y:
+        if y == '>':
+            break
+        else:
+            url = url+y
+    url = url.replace("<a href=","")
+    url = url.replace('"','')
+    stra = f"""Title-{item_count + 1}: {title} \n Url: '{url}' \n"""
+    f.write(stra)
+    f.write('\n')
+    item_count+=1
+print(f"{item_count} Links has been extracted")
+print("file is being saved in 'Extracted Files' folder")
+f.close()
+input()
